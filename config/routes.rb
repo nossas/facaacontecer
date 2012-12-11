397136e040bd53c040a7d2805b1df97a12eb537b
Luiz Fonseca <runeroniek@gmail.com>
2012-12-11 02:11:58 -0200

    Create path names for routes in route's configurations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Selfstarter::Application.routes.draw do
+
+  get   '/preorder'                 => 'preorder#index',    as: :preorder
+  get   '/preorder/checkout',                               as: :checkout
+  get   '/preorder/share/:uuid'     => 'preorder#share',    as: :share
+  post  '/preorder/ipn'             => 'preorder#ipn',      as: :ipn
+  match '/preorder/prefill'         => 'preorder#prefill',  as: :prefill
+  match '/preorder/postfill'        => 'preorder#postfill', as: :postfill 
+
   root :to => 'preorder#index'
-  match '/preorder'               => 'preorder#index'
-  get 'preorder/checkout'
-  match '/preorder/share/:uuid'   => 'preorder#share', :via => :get
-  match '/preorder/ipn'           => 'preorder#ipn', :via => :post
-  match '/preorder/prefill'       => 'preorder#prefill'
-  match '/preorder/postfill'      => 'preorder#postfill'
 end
