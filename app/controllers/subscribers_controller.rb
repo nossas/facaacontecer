5dbb22ebde2518112461cd0a68d2c1c21902b9f1
Luiz Fonseca <runeroniek@gmail.com>
2013-02-24 13:03:53 -0300

    Create subscribers controller
    Create controller that has a User model as resource. Also, add logic
    behavior: if the user has already signed up, with his email and cpf,
    don't throw a error.
    Also add the 'thanks' action - which verifies if the user has
    subscribed; if not, just go home.
    
    TODO: fix the belongs to (it's not working anymore) and remove those
    ugly lines from the create method.

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,38 @@
+class SubscribersController < ApplicationController
+  inherit_resources
+  defaults resource_class: User
+  actions :new, :create 
+
+  respond_to :html, only: :new
+  respond_to :json, only: :create
+  
+
+  before_filter only: [:new] { @token = MOIP_TOKEN }
+
+
+  def create
+    # FIXME: Belongs_to is not working here :/
+    @project = Project.find(params[:project_id])
+
+    # TODO: Remove duplicated code
+    # The subscription url should not be duplicated and should be on the model
+    if @subscriber = User.find_by_email_and_cpf(params[:user][:email], params[:user][:cpf])
+      render json: @subscriber.as_json(
+        subscription_url: project_subscriber_subscriptions_path(@project, @subscriber.id)
+      )
+    else
+      create! do |success, failure|
+        success.json { 
+          render json: @subscriber.as_json(
+            subscription_url: project_subscriber_subscriptions_path(@project, @subscriber.id)
+          ) 
+        }
+      end
+    end
+  end
+
+
+  def thanks
+    redirect_to root_path unless session[:subscriber_ok]
+  end
+end
