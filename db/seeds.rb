15aa104769bdb7e31612287381cad8ea83b2f514
Luiz Fonseca <runeroniek@gmail.com>
2013-03-19 19:41:48 -0300

    Layout changes. Now we're talkin.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,6 @@ project = Project.create!(
   title: 'Project',
   description: %q{Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.},
   goal: 120000.00,
-  video: 'https://www.youtube.com/embed/Ej5rGGTHy54',
+  video: 'http://player.vimeo.com/video/51535113?title=0&amp;byline=0&amp;portrait=0',
   expiration_date: Date.new(2013,2,8)
 )
-
-
