913ff886ac8566c18b979783f94c9b5abded69cc
Luiz Fonseca <runeroniek@gmail.com>
2013-03-24 12:23:48 -0300

    Lower number of workers on heroku.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 # If you have a very small app you may be able to
 # increase this, but in general 3 workers seems to
 # work best
-worker_processes 3
+worker_processes 2
 
 # Load your app into the master before forking
 # workers for super-fast worker spawn times
