955cad09e2e04478eb41f7f36aeaede09de38545
Luiz Fonseca <runeroniek@gmail.com>
2013-03-28 18:39:02 -0300

    Change copy for successful subscriptions. Also, set default subject and url.
    Also, bcc the subscription email.

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,10 +1,15 @@
 class SubscriptionMailer < ActionMailer::Base
-  default from: "Leonardo do Meu Rio <contato@apoie.meurio.org.br>"
+  default from:     "Leonardo do Meu Rio <leonardo@meurio.org.br>",
+          bcc:      'financiador@meurio.org.br',
+          reply_to: 'financiador@meurio.org.br',
+          subject:  "Obrigado por financiar o Meu Rio!",
+          date:     Date.current
 
+  default_url_options[:host] = 'apoie.meurio.org.br'
 
   def successful_create_message(subscriber)
     @subscriber = subscriber
-    mail(to: subscriber.email, subject: "Obrigado por financiar o Meu Rio!")
+    mail(to: subscriber.email)
   end
 
 end
