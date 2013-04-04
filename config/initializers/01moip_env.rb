0fa15321af50e3ad7856ada7190e5cf8d5804eeb
Luiz Fonseca <runeroniek@gmail.com>
2013-04-04 17:02:21 -0300

    Enable production for Moip.

--- config/initializers/01moip_env.rb
+++ config/initializers/01moip_env.rb
@@ -1,10 +1,10 @@
 MOIP_TOKEN  = ENV['MOIP_TOKEN'] 
 
-#if Rails.env.production?
-#  MOIP_URL    = 'https://moip.com.br/assinaturas/v1'
-#  MOIP_JS_URL = '//api.moip.com.br/moip-assinaturas.min.js'
-#else
+if Rails.env.production?
+  MOIP_URL    = 'https://moip.com.br/assinaturas/v1'
+  MOIP_JS_URL = '//api.moip.com.br/moip-assinaturas.min.js'
+else
   MOIP_URL    = 'https://sandbox.moip.com.br/assinaturas/v1'
   MOIP_JS_URL = '//sandbox.moip.com.br/moip-assinaturas.min.js'
-#end
+end
 
