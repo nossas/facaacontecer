e5e8d6d524a974ddce92f4e426044c7196d125f5
Nícolas Iensen <nicolas@iensen.me>
2013-10-09 10:17:06 -0300

    [#56227526] add image to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130711145156) do
+ActiveRecord::Schema.define(:version => 20131009131545) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(:version => 20130711145156) do
     t.string   "phone"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "image"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
