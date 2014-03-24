ca340cd4a1420fd572e125f0909dfd97ee162a7e
Luiz Fonseca <eu@luiz.cc>
2014-03-24 00:17:01 -0300

    Remove duplicated code

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 module ProjectDecorator
-  
+
   def backers_count
     t('backers_html', count: subscribers.count) 
   end
@@ -8,9 +8,9 @@ module ProjectDecorator
 
   def reached
     t('goal.reached_of_html', 
-           current: number_to_currency(revenue, precision: 0), 
-           goal: number_to_currency(goal, :precision => 0)
-      )
+      current: number_to_currency(revenue, precision: 0), 
+      goal: number_to_currency(goal, :precision => 0)
+     )
   end
 
   def days_remaining
@@ -18,11 +18,8 @@ module ProjectDecorator
     now = ((total_days - end_date) * 100)/total_days
     date = end_date == 1 ? t('goal.last_day_remaining_html') : t('goal.days_remaining_html', count: end_date)
 
-    content_tag(
-      :div, content_tag(:div, nil, id: :p_progress, style: "width: #{now}%"),
-      id: :p_progress_bg, class: :small
-    ) + date
-    
+    progress_tag(now) + date
+
   end
 
   def progress
@@ -30,13 +27,16 @@ module ProjectDecorator
     goal    = 1000.00
     now = (current * 100)/goal
 
+    progress_tag(now) +  t('goal.people_so_far_html', days: current)
+  end
+
+
+  def progress_tag(now)
     content_tag(
       :div, content_tag(:div, nil, id: :progress, style: "width: #{now}%"), 
       id: :progress_bg, class: :small
-    ) +
-    t('goal.people_so_far_html', days: current)
+    )
   end
-
 
   def video_embed
     content_tag(:iframe, nil, type: 'text/html', frameborder: 0, width: 512, height: 305, src: video)
