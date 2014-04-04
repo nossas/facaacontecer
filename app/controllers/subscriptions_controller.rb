04d20947e55427a828c74e5b14e393e275ce0c22
Luiz Fonseca <eu@luiz.cc>
2014-04-04 18:41:35 -0300

    Add logic to redirect if the payment is creditcard and when it's not

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,7 +1,7 @@
 class SubscriptionsController < ApplicationController
   before_actions do
     actions(:show, :confirm) { @subscription = Subscription.find_by(id: params[:id]) }
-    actions(:show) { redirect_to payment_path(@subscription.payments.last) if @subscription.waiting? }
+    actions(:show) { redirect_if_not_creditcard }
   end
 
   force_ssl if: :ssl_configured?
@@ -15,5 +15,18 @@ class SubscriptionsController < ApplicationController
   def confirm
     @subscription.wait_confirmation
     render nothing: true
+  end
+
+  
+  # Redirects
+  def redirect_if_not_creditcard
+    # If the subscription is waiting and it's SLIP or DEBIT
+    if @subscription.waiting? && !@subscription.creditcard?
+      redirect_to payment_path(@subscription.payments.last)
+
+    # But if it's creditcard, redirect it already
+    elsif @subscription.creditcard?
+      redirect_to payment_path(@subscription.payments.last) 
+    end
   end
 end
