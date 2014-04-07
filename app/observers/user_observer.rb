13a5c3c62ad799b0150388ea444e4d64818c4503
Luiz Fonseca <eu@luiz.cc>
2014-04-06 21:11:54 -0300

    Use a trick to generate ids correctly when using meurio_accounts service.

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -4,8 +4,18 @@ module UserObserver
 
   included do
     # Build invite when creating a new user
-    before_create { self.build_invite } 
+    before_create :find_next_val_for_id, if: -> { Rails.env.production? }
+    after_create { self.invite.create! } 
 
+
+    def find_next_val_for_id
+     next_id = raw_sql %Q{select (id + 1) from users order by id desc limit 1}
+     self.id = next_id.to_i
+    end
+
+    def raw_sql(sql)
+      ActiveRecord::Base.connection.execute(sql).to_a.first["id"]
+    end
 
   end
 end
