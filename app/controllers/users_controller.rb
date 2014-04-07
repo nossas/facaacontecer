9d98fbaf0b0e8f3069709725443b1b8aace36c89
Luiz Fonseca <eu@luiz.cc>
2014-04-07 09:36:02 -0300

    Remove bold font from email's user first name and add comments.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,32 +23,38 @@ class UsersController < ApplicationController
   def create 
 
     case 
-    when @user.new_record?
-      @user.save 
-    when !@user.new_record?
-      @user.try(:update_attributes, user_params)
+      # If the user is a new user, try to save it
+      when @user.new_record?
+        @user.save 
+
+      # if the user has already an account (with its email)
+      # try to update it
+      when !@user.new_record?
+        @user.try(:update_attributes, user_params)
     end
-    
-   
+
+    # Render the form if errors are present in the user (update or save)
     return render :create if @user.errors.any?
+
+    # Or redirect_to the subscription path if nothing wrong
     redirect_to subscription_path(@user.subscriptions.last)
-    
+
   end
 
 
-    private
-    # Checking for user params on request
-    def user_params 
-      if params[:user]
-        params.require(:user).permit(
-          %i(first_name last_name email cpf birthday 
+  private
+  # Checking for user params on request
+  def user_params 
+    if params[:user]
+      params.require(:user).permit(
+        %i(first_name last_name email cpf birthday 
                 postal_code address_street address_extra 
                 address_number address_district city state 
                 phone country), 
                 :subscriptions_attributes => [:value, :plan, :payment_option, :project_id, :bank]
-        )
-      else
-        {}
-      end
+      )
+    else
+      {}
     end
   end
+end
