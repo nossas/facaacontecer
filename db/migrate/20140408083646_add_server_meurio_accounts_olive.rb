410515f4d35b5de2fde003af1af69e58e513285d
Nícolas Iensen <nicolas@iensen.me>
2014-04-08 07:25:42 -0300

    Fix migration

--- db/migrate/20140408083646_add_server_meurio_accounts_olive.rb
+++ db/migrate/20140408083646_add_server_meurio_accounts_olive.rb
@@ -9,7 +9,7 @@ class AddServerMeurioAccountsOlive < ActiveRecord::Migration
       raise "MEURIO_ACCOUNTS_DBPORT is missing" if ENV["MEURIO_ACCOUNTS_DBPORT"].nil?
       raise "DB_USERNAME is missing" if ENV["DB_USERNAME"].nil?
       execute "CREATE EXTENSION postgres_fdw;"
-      execute "CREATE SERVER meurio_accounts FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname '#{ENV["MEURIO_ACCOUNTS_DBNAME"]}', host '#{ENV["MEURIO_ACCOUNTS_DBHOST"]}', port: '#{ENV["MEURIO_ACCOUNTS_DBPORT"]}');"
+      execute "CREATE SERVER meurio_accounts FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname '#{ENV["MEURIO_ACCOUNTS_DBNAME"]}', host '#{ENV["MEURIO_ACCOUNTS_DBHOST"]}', port '#{ENV["MEURIO_ACCOUNTS_DBPORT"]}');"
       execute "CREATE USER MAPPING for #{ENV["DB_USERNAME"]} SERVER meurio_accounts OPTIONS (user '#{ENV["MEURIO_ACCOUNTS_DBUSER"]}', password '#{ENV["MEURIO_ACCOUNTS_DBPASS"]}');"
     end
   end
