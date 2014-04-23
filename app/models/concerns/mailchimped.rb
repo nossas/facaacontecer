fdd466201364c4931e6dc1148965121b477b85e6
Nícolas Iensen <nicolas@iensen.me>
2014-04-23 09:30:18 -0300

    [#69955324] add updated at column of subscritpion to the MC

--- app/models/concerns/mailchimped.rb
+++ app/models/concerns/mailchimped.rb
@@ -46,6 +46,7 @@ module Mailchimped
           ADMISSION: self.created_at.strftime("%m/%d/%Y"),
           NDONATIONS: self.successful_invoices.size,
           LDONATION: self.last_successful_invoice_date.try(:strftime, "%m/%d/%Y"),
+          SUBUPDATED: self.updated_at.strftime("%m/%d/%Y"),
           VALUE: self.value
         }
       )
