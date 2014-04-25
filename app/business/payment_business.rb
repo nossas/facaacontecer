68f6fdff0305aaeb5fe75826c38840d74da0c282
Nícolas Iensen <nicolas@iensen.me>
2014-04-25 09:55:55 -0300

    [#70090144] fix PaymentBusiness instruction method

--- app/business/payment_business.rb
+++ app/business/payment_business.rb
@@ -4,15 +4,15 @@ module PaymentBusiness
   included do
 
     # Default instruction object
-    def instruction 
+    def instruction
       MyMoip::Instruction.new(
         # generate an unique identifier for this instruction
         # adding _payment suffix to it.
-        id:             "#{code}PAYMENT",
+        id:             "#{code}_#{self.payments.count}_PAYMENT",
         payment_reason: "Doação para o MeuRio - http://meurio.org.br",
         values:         [value],
         payer:          user.business.build_payer,
-        #return_url:    '', 
+        #return_url:    '',
         #notification_url: Rails.application.routes.url_helpers.notifications_payments_url(host: MOIP_NOTIFICATIONS_HOST),
       )
     end
@@ -24,23 +24,23 @@ module PaymentBusiness
 
       return transparent
     end
- 
+
 
     # Payment Request status
     def success?
       payment_request.success?
     end
-   
+
 
     # Return the PAYMENT URL (Boleto, Debito) url
     def url
       # Payment_Request is implemented on each PaymentMethod class
       payment_request.url
     end
-  
+
 
     def token
-      # Getting the token for boleto/debito 
+      # Getting the token for boleto/debito
       payment_request.token
     end
 
