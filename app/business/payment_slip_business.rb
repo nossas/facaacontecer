7aea0d79ba46ec5a0a604db925ac661162e7356b
Nícolas Iensen <nicolas@iensen.me>
2014-04-25 10:16:38 -0300

    [#70090144] fix PaymentSlipBusiness code for request

--- app/business/payment_slip_business.rb
+++ app/business/payment_slip_business.rb
@@ -2,17 +2,17 @@ module PaymentSlipBusiness
   extend ActiveSupport::Concern
 
   included do
-   
+
     # Build the payment slip request
-    def payment_request 
+    def payment_request
       payment_slip_payment = MyMoip::PaymentSlipPayment.new
-      payment_request      = MyMoip::PaymentRequest.new(code)
+      payment_request      = MyMoip::PaymentRequest.new("#{code}_#{self.payments.count}_PAYMENT")
       payment_request.api_call(payment_slip_payment, token: transparent_request.token)
 
       return payment_request
     end
-    
+
   end
- 
+
 
 end
