438595c25e528b2d08c8713b93ebb10917d6bd16
Nícolas Iensen <nicolas@iensen.me>
2014-05-16 16:00:12 -0300

    Fix dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,5 @@ Selfstarter::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
 
-  config.default_url_options[:host] = "localhost:3000"
+  default_url_options[:host] = "localhost:3000"
 end
