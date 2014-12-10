4cb4c83ac89386c0d974cf472413e49faf430ec3
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-12-10 18:02:07 -0200

    Add organization avatar to the radius buttons

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,9 @@
 class Organization < ActiveRecord::Base
+  def avatar_url
+    if self.avatar
+      "https://#{ENV['ACCOUNTS_BUCKET']}.s3.amazonaws.com/uploads/organization/avatar/#{self.id}/square_#{self.avatar}"
+    else
+      "http://i.imgur.com/lsAFCHL.jpg"
+    end
+  end
 end
