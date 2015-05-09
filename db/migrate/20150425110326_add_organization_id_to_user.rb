9f867d8af41d2ae76d7885d2d8ad46dd36855c1a
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-05-09 07:43:06 -0300

    Fixed migration

--- db/migrate/20150425110326_add_organization_id_to_user.rb
+++ db/migrate/20150425110326_add_organization_id_to_user.rb
@@ -1,5 +1,5 @@
 class AddOrganizationIdToUser < ActiveRecord::Migration
   def change
-    add_column :users, :organization_id, :integer
+    add_column :users, :organization_id, :integer, foreign_key: false
   end
 end
